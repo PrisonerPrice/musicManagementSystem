e0b337b8e4de71a25000557443bca6d44205b03c
Hangbo <zhzhanghangbo@hotmail.com>
2019-12-23 12:01:01 -0500

    Add logger filter and util class Functions

--- src/main/java/com/prisonerprice/init/AppInitializer.java
+++ src/main/java/com/prisonerprice/init/AppInitializer.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 
 @SpringBootApplication(scanBasePackages = {"com.prisonerprice"})
+@ServletComponentScan(basePackages = {"com.prisonerprice.filter"})
 public class AppInitializer {
     public static void main(String[] args) throws NullPointerException{
         if (HibernateUtil.getSessionFactory() == null) {
